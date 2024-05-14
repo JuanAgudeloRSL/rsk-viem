aae852410ec52e63e307aac4701beb43c627396e
JuanDiego <juan.agudelo@ext.rootstocklabs.com>
2024-05-14 08:54:47 -0500

    🚧 Sandbox

--- src/config/clients.ts
+++ src/config/clients.ts
@@ -3,16 +3,16 @@ import { RskTestnet } from '../utils/rskTestnet';
 import { privateKeyToAccount } from 'viem/accounts';
 import dotenv from 'dotenv';
 
-dotenv.config
+dotenv.config()
 
-const PRIVATE_KEY = process.env.WALLET_ADDRESS;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 
 if (!PRIVATE_KEY) {
     throw new Error('Private key is not provided. Please set the environment variable PRIVATE_KEY.');
 }
 
-console.log(PRIVATE_KEY)
+
 const account = privateKeyToAccount(`0x${process.env.PRIVATE_KEY}`);
 
 export const PublicClient = createPublicClient({
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { WalletClient } from "./config";
-
+import { ERC1155 } from "./contracts/ERC1155";
 
 
 
 // Testing
 async function call() {
     try {
-        console.log(WalletClient)
+        const mint = ERC1155.write.mint(['0x08C4E4BdAb2473E454B8B2a4400358792786d341',1])
+        console.log(await mint)
     } catch (error) {
         console.error('Failed to call getBts:', error);
     }
